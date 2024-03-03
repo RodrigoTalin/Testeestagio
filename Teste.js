6d36a6d526534c3c7e68ef68b0cb635255c24a4b
Rodrigo Alef <161965166+RodrigoTalin@users.noreply.github.com>
2024-03-03 12:07:31 -0300

    Update Teste.js

---
+++ Teste.js
@@ -0,0 +1,14 @@
+var containerElement = document.getElementById('panel_nutriscore');
+var h4element = containerElement.querySelector('h4');
+var infoText = h4element.textContent;
+console.log(infoText);
+
+var containerElement = document.getElementById('panel_nova');
+var h4element = containerElement.querySelector('h4');
+var infoText = h4element.textContent;
+console.log(infoText);
+
+var containerElement = document.getElementById('panel_ecoscore');
+var h4element = containerElement.querySelector('h4');
+var infoText = h4element.textContent;
+console.log(infoText);
